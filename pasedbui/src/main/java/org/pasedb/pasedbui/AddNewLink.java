21c93f5efeed0f2e24578190049639c08a8fb151
Steve Gee <steve.gee@sprint.com>
2019-06-06 16:32:07 -0500

    adding new add-link code to actually persist the sties & links

--- pasedbui/src/main/java/org/pasedb/pasedbui/AddNewLink.java
+++ pasedbui/src/main/java/org/pasedb/pasedbui/AddNewLink.java
@@ -1,6 +1,15 @@
 package org.pasedb.pasedbui;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
+
+import java.awt.image.BufferedImage;
+import java.net.URL;
+import javax.imageio.ImageIO;
+//import org.jsoup.Connection.Response;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
@@ -8,6 +17,9 @@ import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+
+import java.awt.image.BufferedImage;
+import java.net.URL;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Date;
@@ -59,5 +71,59 @@ public class AddNewLink {
 			e1.printStackTrace();
 		}
 	}
-
+	
+	public LinkItem fetchOGMetaData(String url) throws Exception{
+		LinkItem ogi = new LinkItem();
+		Document document = Jsoup.connect(url).get();
+	    String title = null;
+	    Elements metaOgTitle = document.select("meta[property=og:title]");
+	    if (metaOgTitle!=null) {
+	        title = metaOgTitle.attr("content");
+	    }
+	    else {
+	        title = document.title();
+	    }
+	    String desc = null;
+	    Elements metaOgDesc = document.select("meta[property=og:description]");
+	    if (metaOgTitle!=null) {
+	    	desc = metaOgDesc.attr("content");
+	    }else {
+	    	desc = "missing og:description";
+	    }
+	    
+	    String imageUrl = null;
+	    Elements metaOgImage = document.select("meta[property=og:image]");
+	    if (metaOgImage!=null) {
+	    	imageUrl = metaOgImage.attr("content");
+//	    	System.out.println("imageUrl: " + imageUrl);
+//	    	System.out.println("length: " + imageUrl.trim().length());
+	    	if(imageUrl != null && imageUrl.trim().length() > 4){
+		    	try{
+					URL iurl=new URL(imageUrl);
+					BufferedImage image = ImageIO.read(iurl);
+					int height = image.getHeight();
+					int width = image.getWidth();
+					System.out.println("Image Height : "+ height);
+					System.out.println("Image Width : "+ width);
+					// add the code to make the image smaller here -- revision 2, no need to work on this now!
+		    	}catch(java.net.MalformedURLException mu){
+		    		mu.printStackTrace();
+		    	}
+	    	}
+	    }
+	    
+//	    URL:  max length than 256 characters
+	    System.out.println("URL: " + url);
+//	    title max length than 100 characters
+	    System.out.println("Title: " + title);
+//	    description max length than 200 characters
+	    System.out.println("Description: " + desc);
+//	    Image url max length than 256 characters
+	    System.out.println("Image: " + imageUrl);
+	    ogi.setUrl(url);
+	    ogi.setImgurl(imageUrl);
+	    ogi.setTitle(title);
+	    ogi.setDescription(desc);
+	    return ogi;
+	}
 }
