cb61109a76c3b66401b369e3e9bdd41108ad93ef
Steve Gee <steve.gee@sprint.com>
2019-06-12 10:31:03 -0500

    mintor html tweaks

--- pasedbui/src/main/java/org/pasedb/pasedbui/HtmlEngine.java
+++ pasedbui/src/main/java/org/pasedb/pasedbui/HtmlEngine.java
@@ -24,11 +24,13 @@ public class HtmlEngine {
 		sb.append("<tbody><tr><td>");
 		sb.append("<img src=\"" + li.getImgurl() + "\" height=\"" + li.getDisplayHeight() + "\"  width=\"" + li.getDisplayWidth() + "\"><br>");
 		sb.append("</td></tr><tr><td>");
-		sb.append("<b>Title: </b>" + li.getTitle());
-		sb.append("</td></tr><tr><td>");
-		sb.append("<b>Description: </b>" + li.getDescription());
-		sb.append("</td></tr><tr><td>");
-		sb.append("<b>Comments: </b>" + li.getComment());
+//		sb.append("<b>Title: </b>" + li.getTitle());
+		sb.append("<b>" + li.getTitle());
+		sb.append("</b></td></tr><tr><td>");
+//		sb.append("<b>Description: </b>" + li.getDescription());
+		sb.append("<b>" + li.getDescription());
+		sb.append("</b></td></tr><tr><td>");
+		sb.append("<b>{username}: </b>" + li.getComment());
 		sb.append("</td></tr></tbody>");
 		return sb.toString();
 	}
