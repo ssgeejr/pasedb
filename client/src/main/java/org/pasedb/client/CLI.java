bafdfea445f3afcf4ffcb7fea387db93f7eb9d31
ioexcept <ioexcept@gmail.com>
2019-07-20 21:12:55 -0500

    added a new cli client that will be used to import single/bulk URL's

---
+++ client/src/main/java/org/pasedb/client/CLI.java
@@ -0,0 +1,13 @@
+package org.pasedb.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class CLI 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
