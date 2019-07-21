ef2475e1b53a3b42cec22a91aa926541b64bddf0
ioexcept <ioexcept@gmail.com>
2019-07-20 22:10:04 -0500

    working test model

--- client/src/main/java/org/pasedb/client/CLI.java
+++ client/src/main/java/org/pasedb/client/CLI.java
@@ -1,13 +1,150 @@
 package org.pasedb.client;
 
-/**
- * Hello world!
- *
- */
-public class CLI 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import javax.imageio.ImageIO;
+
+import java.awt.image.BufferedImage;
+import java.net.URL;
+import javax.imageio.ImageIO;
+//import org.jsoup.Connection.Response;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.pasedb.pasedbui.LinkItem;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
+import java.awt.image.BufferedImage;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import com.google.common.collect.Multiset;
+import com.google.common.collect.Multisets;
+import com.google.common.collect.TreeMultiset;
+
+import java.math.BigDecimal;
+
+public class CLI {
+	private final int MAX_XY = 200;
+
+	
+	public CLI() {
+		try {
+			LinkItem ogi = fetchOGMetaData("http://pasedb.org","comment", new ArrayList<Integer>(),-1);
+			System.out.println(ogi.getTitle());
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	
+	public LinkItem fetchOGMetaData(String url, String comment, ArrayList<Integer> tags, int userID) throws Exception{
+		LinkItem ogi = new LinkItem();
+		Document document = Jsoup.connect(url).get();
+	    String title = null;
+	    Elements metaOgTitle = document.select("meta[property=og:title]");
+	    if (metaOgTitle!=null) {
+	        title = metaOgTitle.attr("content");
+	    }
+	    if (title == null || title.length() < 3){
+	        title = document.title();
+	    }
+	    if (title == null || title.length() < 3){
+	    	title = url;
+	    }
+	    String desc = null;
+	    Elements metaOgDesc = document.select("meta[property=og:description]");
+	    if (metaOgTitle!=null) {
+	    	desc = metaOgDesc.attr("content");
+	    }else {
+	    	desc = "No description available";
+	    }
+	    
+	    String imageUrl = null;
+	    Elements metaOgImage = document.select("meta[property=og:image]");
+	    if (metaOgImage!=null) {
+	    	imageUrl = metaOgImage.attr("content");
+//	    	System.out.println("imageUrl: " + imageUrl);
+//	    	System.out.println("length: " + imageUrl.trim().length());
+	    	if(imageUrl != null && imageUrl.trim().length() > 4){
+		    	try{
+					URL iurl=new URL(imageUrl);
+					BufferedImage image = ImageIO.read(iurl);
+					int height = image.getHeight();
+					int width = image.getWidth();
+					System.out.println("Image Height : "+ height);
+					System.out.println("Image Width : "+ width);
+					float pct = setPct(height,width);
+					int display_height = Math.round(height * pct);
+					int display_width = Math.round(width * pct);
+					System.out.println("Display Height : "+ display_height);
+					System.out.println("Display Width : "+ display_width);
+					ogi.setImgurl(imageUrl);
+					ogi.setDisplayHeight(display_height);
+					ogi.setDisplayWidth(display_width);
+					// add the code to make the image smaller here -- revision 2, no need to work on this now!
+		    	}catch(java.net.MalformedURLException mu){
+		    		mu.printStackTrace();
+		    	}
+	    	}
+	    }
+	    
+//	    URL:  max length than 256 characters
+	    System.out.println("URL: " + url);
+//	    title max length than 100 characters
+	    System.out.println("Title: " + title);
+//	    description max length than 200 characters
+	    System.out.println("Description: " + desc);
+//	    Image url max length than 256 characters
+	    System.out.println("Image: " + imageUrl);
+	    System.out.println("comment: " + comment);
+	    System.out.println("userID: " + userID);
+	    
+	    ogi.setUrl(url);
+	    ogi.setTitle(title);
+	    ogi.setDescription(desc);
+	    ogi.setComment(comment);
+	    ogi.setUserID(userID);
+	    ogi.setTags(tags);
+	    	    
+//	    try{	    	
+//	    	persist(ogi);
+//	    	
+//	    }catch(Exception ex){
+//	    	System.out.println("FAILED TO INSERT RECORD");
+//	    	ex.printStackTrace();
+//	    }
+	    
+	    return ogi;
+	}
+	
+	
+	    	private float setPct(int height, int width) {
+	    		double max = new Double(height).doubleValue();
+	    		if (width > height) max = new Double(width).doubleValue();
+	    		if (max <= MAX_XY) return 1;
+	    		double val = MAX_XY / max; 
+	    	    BigDecimal bd = new BigDecimal(Double.toString(val));
+	    	    bd = bd.setScale(2, BigDecimal.ROUND_HALF_UP);       
+//	    		System.out.println("width: " + width);
+//	    		System.out.println("height: " + height);
+//	    		System.out.println("max: " + max);
+//	    		System.out.println("MAX_XY: " + MAX_XY);m 
+//	    		System.out.println("val: " + val);
+//	    		System.out.println("bd: " + bd.floatValue());
+	    	    return bd.floatValue();
+	    	}
+	    	
+	
+	public static void main(String[] args) {
+		new CLI();
+	}
 }
