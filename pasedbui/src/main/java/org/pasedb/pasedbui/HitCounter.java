1f2d4532c47000e54115fa82c8afca1394a48724
sys admin <develop@pasedb.org>
2021-03-23 07:47:22 -0500

    updated hit counter to correctly sort the dates

--- pasedbui/src/main/java/org/pasedb/pasedbui/HitCounter.java
+++ pasedbui/src/main/java/org/pasedb/pasedbui/HitCounter.java
@@ -19,10 +19,10 @@ public class HitCounter {
 			Class.forName("com.mysql.cj.jdbc.Driver");
  			conn = DriverManager.getConnection("jdbc:mysql://pasedb:3306/pasedb?user=pasedb&password=alienation"); 
  			PreparedStatement hc=conn.prepareStatement("select\n" + 
- 					" DATE_FORMAT(counter_date, \"%m-%d-%Y\") as a, count(*) as b" + 
+ 					" DATE_FORMAT(counter_date, \"%Y-%m-%d\") as a, count(*) as b" + 
  					" from counter\n" + 
- 					" group by DATE_FORMAT(counter_date, \"%m-%d-%Y\")" + 
- 					" order by DATE_FORMAT(counter_date, \"%m-%d-%Y\") desc");
+ 					" group by a" +
+ 					" order by a desc");
  			ResultSet rs = hc.executeQuery();
  			int count = 0;
  			while(rs.next()) {
